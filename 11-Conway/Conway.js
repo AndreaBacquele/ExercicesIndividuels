c531c44980697dcb0eaf9f28165824bd4fc62186
Andréa Bacquelé <bacquelea@gmail.com>
2023-05-17 16:46:29 +0200

    Etape 3 validée

--- 11-Conway/Conway.js
+++ 11-Conway/Conway.js
@@ -1,3 +1,4 @@
+// Etape 1
 //Prend en paramétre une string et renvoie la même string dans laquelle les caractéres successifs non identiques sont séparés par un espace
 function decoupeChaine(string){
     let string2 = string[0]
@@ -12,6 +13,9 @@ function decoupeChaine(string){
 }
 
 //console.log(decoupeChaine("aaaabcabbbc"))
+
+//Etape 2
+//Créer une fonction decritChaine, inspirée de decoupeChaine, qui prend en paramètre une string et renvoie une string qui décrit, tel qu'expliqué ci-dessus, les caractères qui constituent la chaîne en paramètre.
 
 function decritChaine(string) {
     let compteur = 1;
@@ -30,5 +34,26 @@ function decritChaine(string) {
     return string2;
   }
 
-console.log(decritChaine("ab"))   // renvoie "1a1b"
-console.log(decritChaine("aabbca"))  // renvoie "2a2b1c1a"
+// console.log(decritChaine("ab"))   // renvoie "1a1b"
+// console.log(decritChaine("aabbca"))  // renvoie "2a2b1c1a"
+
+
+//Etape 3
+//Créer une fonction suiteConway qui donne les n premiers termes de la suite qui commence par le caractère carac. n et carac sont passés en paramètres de la fonction.
+
+function suiteConway(carac,n){
+    console.log(carac)
+    for (let i = 1; i<n; i++){
+    carac = decritChaine(carac)
+    console.log(carac);
+}
+}
+suiteConway('a', 3)
+// a
+// 1a
+// 111a
+
+suiteConway('1', 3)  
+// 1
+// 11
+// 21
