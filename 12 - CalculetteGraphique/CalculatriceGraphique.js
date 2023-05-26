094572aa7e9a583a7b390a187b0069bbf1dfcd0e
Andréa Bacquelé <bacquelea@gmail.com>
2023-05-26 16:04:59 +0200

    MAJ de l'exercice calculatrice + Exercice sur le tri

---
+++ 12 - CalculetteGraphique/CalculatriceGraphique.js
@@ -0,0 +1,16 @@
+function stockValueOfButton(n){
+    let number = document.getElementById("chiffre").innerHTML += n;
+    const calcul = eval(chiffre.textContent);
+    console.log(calcul)
+    number = document.getElementById("chiffre").innerHTML = calcul
+    return number
+}
+
+function test(){
+    let number = document.getElementsByClassName("number")
+    console.log(number)
+}
+
+test()
+
+
